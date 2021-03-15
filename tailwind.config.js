d67d619263d05f7018c00e33568e14117cc12c17
rncongi <rncongi@gmail.com>
2021-03-15 09:56:11 -0400

    Added custom font and primary colors in TW config.

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#272838', 
+        secondary: {
+          100: '#F3DE8A',
+          200: '#E8C021',
+        }
+      },
+      fontFamily: {
+        body: ['Gochi Hand']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
