2400bfc6b8745e497455f944246b3aed531159c5
rncongi <rncongi@gmail.com>
2021-03-20 13:39:44 -0400

    changed categories and added filler color

---
+++ NOTES.md
@@ -0,0 +1,7 @@
+to do:
+* add placeholder text color
+* fix height issue on image (custom heights?)
+
+Stretch:
+* make comic image an upload from computer
+* add 'likes' badge/attribute to comics 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
         main: '#FFFDF3', 
         accent: '#FEB098',
         pop: '#ED4F3F',
-        ground: '#182335'
+        ground: '#182335',
+        filler: '#4d6a99'
       },
       fontFamily: {
         body: ['Gochi Hand']
